af859229eaf9eb3c8e5225dbf51113bc4384c0d9
opilane <opilane@HP-CZC6297BB4>
2021-11-23 16:05:33 +0200

    Add project files.

---
+++ Контрольная_работа_по_теме_Циклы_В._3.py
@@ -0,0 +1,62 @@
+#1
+#from random import*
+#n=randint(1,9)
+#for i in range(n):
+#    print(" ----- ",end=" ")
+#print()
+#for i in range(n):
+#    print("|  O  |",end=" ")
+#print()
+#for i in range(n):
+#    print("!  -  !",end=" ")
+#print()
+#for i in range(n):
+#    print(" ----- ",end=" ")
+#print()
+
+#2
+#natur=input("Введите число: ")
+#step=input("Введите показатель степени: ")
+#step=0
+#natur=0
+#while True and step<=0 or natur<=0: #По какой-то причине не работает проверка на сравнение чисел с 0
+#  try:
+#     natur=int(input("Введите число: "))  
+#     step=int(input("Введите показатель степени: ")) 
+#  except ValueError:
+#     print("Вы ввели не натуральное число или это буква, введите еще раз")
+#     continue
+#  else:
+#     break 
+#for i in range(1,natur+1):
+#    arv=i**step
+#    print(arv)
+
+#3
+#from random import*
+#õ=randint(1,100)
+#h=0
+#for i in range(õ):
+#    h+=randint(1,5)
+#x=h/õ
+#g=round(x,2)
+#print(f"Средняя оценка {õ} учеников: {g}")
+
+#4
+#dol=0
+#for i in range(100):
+#    dol+=i
+#    if dol>=100:
+#        print(f"К {i} дню рождению будет {dol}$")
+#        break
+
+#5
+#fib1=fib2=1
+#print(fib1,fib2, end=" ")
+#for i in range(6):
+#    fib1,fib2=fib2,fib1+fib2
+#    print(fib2, end=" ")
+
+
+
+
